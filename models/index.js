babb3268b8468b25c56c5fec86e256dc1e37667b
Yuri <uriahmccarthy@gmail.com>
2023-03-29 18:51:43 -0700

    Updated Server Routes and ReadMe

--- models/index.js
+++ models/index.js
@@ -10,19 +10,19 @@ Product.belongsTo(Category, {
   onDelete: 'CASCADE' // Parent record - chid records dropped
 })
 // Categories have many Products
-Category.belongsTo(Product, {
+Category.hasMany(Product, {
   foreignKey : 'category_id',
   
 })
 //P - T many-many
 // Products belongToMany Tags (through ProductTag)
-Product.belongsTo(Tag, {
+Product.belongsToMany(Tag, {
   foreignKey : 'product_id',
   through: ProductTag
 })
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsTo(Product, {
+Tag.belongsToMany(Product, {
   foreignKey : 'tag_id',
   through: ProductTag
 })
